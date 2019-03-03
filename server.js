6dc2bd1ee9daae5aefb7df3905fa071e2b0b1a38
Naman Arora <36225548+palindrome69@users.noreply.github.com>
2019-03-03 15:02:30 +0530

    server: add middlewares for auth and routes support

--- server.js
+++ server.js
@@ -1,10 +1,48 @@
-const express = require('express')
+const express = require('express');
+const connect = require('./utils/mongoose');
+const users = require('./routes/users');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-app = express();
+var app = express();
 
-app.get('/', (req, res) => {
-    res.send("<h1>Hello, We'll be making RESTFul APIs so this won't be used</h1>");
+// connect to database
+connect((err, name) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Successfully connected to database " + name);
+    }
+});
+
+// logger
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url} ${req.ip}`);
+    next();
+});
+
+// cors
+app.use(cors());
+
+// post data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// static
+app.use('/static', express.static('public'));
+
+// routes
+app.use('/user', users)
+
+// 404
+app.use((req, res, next) => {
+    return res.status(404).json({
+        message: "Couldn't find the resource",
+        url: req.url
+    });
 });
 
 const port = process.env.PORT || 8000;
-app.listen(port, 'localhost')
+app.listen(port, 'localhost');
