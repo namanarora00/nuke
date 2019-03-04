a6321009ccf0f4d68eb06e7f6786226b8212951c
Naman Arora <36225548+palindrome69@users.noreply.github.com>
2019-03-04 19:48:08 +0530

    fixes

--- server.js
+++ server.js
@@ -56,5 +56,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 
-const port = process.env.PORT || 8000;
-app.listen(port, 'localhost');
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log("starting server")
+});
