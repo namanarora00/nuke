c6fb10b33bf06efc0b863e818f1931ea58a0e22c
Naman Arora <36225548+palindrome69@users.noreply.github.com>
2019-03-21 14:46:03 +0530

    frontend: basic routing setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LandingPage from './layouts/landing'
+import App from "./App"
 import './main.css'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render( < LandingPage / > , document.getElementById('root'));
+ReactDOM.render( < App / > , document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
